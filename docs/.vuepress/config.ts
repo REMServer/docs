85b6f62066b0d104af87e066eb94b6f2d23d5731
Nofated <49985975+Nofated095@users.noreply.github.com>
2021-09-09 21:09:48 +0800

    Create config.ts

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,139 @@
+import { defineUserConfig } from '@vuepress/cli'
+import type { DefaultThemeOptions } from '@vuepress/theme-default'
+import { path } from '@vuepress/utils'
+import { navbar, sidebar } from './configs'
+
+const isProd = process.env.NODE_ENV === 'production'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  base: '/',
+
+  head: [
+    ["link",{ rel: "icon",href: "/assets/logo/darklogo.png" }]
+  ],
+    ['meta', { name: 'msapplication-TileColor', content: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+  ],
+
+  // site-level locales config
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'Redstone Experimental Server',
+      description: '一个国内不知名的红石技术向生存服',
+    },
+  },
+
+  themeConfig: {
+    logo: '/assets/logo/darklogo.png',
+
+    repo: 'REMSMC/docs',
+
+    docsDir: 'docs',
+
+    // theme-level locales config
+    locales: {
+      /**
+       * Chinese locale config
+       */
+      '/': {
+        // navbar
+
+        // sidebar
+        sidebar: sidebar.zh,
+
+        // page meta
+        editLinkText: '在 GitHub 上编辑此页',
+        lastUpdatedText: '上次更新',
+        contributorsText: '贡献者',
+
+        // custom containers
+        tip: '提示',
+        warning: '注意',
+        danger: '警告',
+
+        // 404 page
+        notFound: [
+          '这里什么都没有',
+          '我们怎么到这来了？',
+          '这是一个 404 页面',
+          '看起来我们进入了错误的链接',
+        ],
+        backToHome: '返回首页',
+
+        // a11y
+        openInNewWindow: '在新窗口打开',
+        toggleDarkMode: '切换夜间模式',
+        toggleSidebar: '切换侧边栏',
+      },
+    },
+
+    themePlugins: {
+      // only enable git plugin in production mode
+      git: isProd,
+    },
+  },
+
+  markdown: {
+    importCode: {
+      handleImportPath: (str) =>
+        str.replace(
+          /^@vuepress/,
+          path.resolve(__dirname, '../../packages/@vuepress')
+        ),
+    },
+  },
+
+  plugins: [
+    ['@vuepress/plugin-debug'],
+    [
+      '@vuepress/plugin-docsearch',
+      {
+        apiKey: '3a539aab83105f01761a137c61004d85',
+        indexName: 'vuepress',
+        searchParameters: {
+          facetFilters: ['tags:v2'],
+        },
+        locales: {
+          '/zh/': {
+            placeholder: '搜索文档',
+          },
+        },
+      },
+    ],
+    [
+      '@vuepress/plugin-google-analytics',
+      {
+        // we have multiple deployments, which would use different id
+        id: process.env.DOCS_GA_ID,
+      },
+    ],
+    ['@vuepress/plugin-pwa'],
+    [
+      '@vuepress/plugin-pwa-popup',
+      {
+        locales: {
+          '/zh/': {
+            message: '发现新内容可用',
+            buttonText: '刷新',
+          },
+        },
+      },
+    ],
+    [
+      '@vuepress/plugin-register-components',
+      {
+        componentsDir: path.resolve(__dirname, './components'),
+      },
+    ],
+    // only enable shiki plugin in production mode
+    [
+      '@vuepress/plugin-shiki',
+      isProd
+        ? {
+            theme: 'dark-plus',
+          }
+        : false,
+    ],
+  ],
+})
